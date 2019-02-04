f53c1aeb9e9abd032fc04130a4d7a9221971c398
coder-xiaoyi <237972113@qq.com>
2019-02-04 21:19:07 +0800

    添加默认.NET Core MVC项目

---
+++ SCHOOL-QDMS/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SCHOOL_QDMS.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
